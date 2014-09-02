5d2491690fa42bdc70223a508a99232b5d18c5de
asonnenschein <adrian.sonnenschein@gmail.com>
2014-09-01 17:25:43 -0700

    initial commit, took most of this out of ckanext-geoserver

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.0.1'
+
+setup(
+    name='ckanext-ogcpreview',
+    version=version,
+    description="Functionality for previewing OGC services",
+    long_description='''
+    ''',
+    classifiers=[],
+    keywords='',
+    author='Arizona Geological Survey',
+    author_email='adrian.sonnenschein@azgs.az.gov',
+    url='http://geothermaldata.org',
+    license='MIT',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    namespace_packages=['ckanext', 'ckanext.ogcpreview'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[],
+    entry_points=\
+    """
+    [ckan.plugins]
+    ogcpreview=ckanext.csw.plugin:DatastoreCSW
+    """,
+)
